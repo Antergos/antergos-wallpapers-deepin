3f31febabfebdcced8a9de67c70f00dc9fb7442f
karasu <karasu@antergos.com>
2017-07-11 12:51:53 +0200

    Add xml description file

---
+++ create_xml.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+import os
+
+file_names = []
+for dpath, d, files in os.walk('.'):
+    for fname in files:
+        if fname.endswith(".jpg") or fname.endswith(".png"):
+            file_names.append(fname)
+
+with open("antergos-deepin-backgrounds-4-3.xml", "w") as xml:
+    xml.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    xml.write('<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">\n')
+    xml.write('<wallpapers>\n')
+
+    for name in file_names:
+        xml.write('<wallpaper>\n')
+        human_name = name[:-4]
+        human_name = human_name.replace("-", " ")
+        human_name = human_name.replace("_", " ")
+        xml.write('<name>{}</name>\n'.format(human_name))
+        xml.write('<filename>/usr/share/antergos/wallpapers-deepin/{}</filename>\n'.format(name))
+        xml.write('<options>zoom</options>\n')
+        xml.write('<pcolor>#ffffff</pcolor>\n')
+        xml.write('<scolor>#ffffff</scolor>\n')
+        xml.write('<shade_type>solid</shade_type>\n')
+        xml.write('</wallpaper>\n\n')
+
+    xml.write('</wallpapers>\n')
